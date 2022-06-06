883e3a011c760380588ee9f07510db8032a2e7e3
rafaelmoreira9719 <rafael.lima@sptech.school>
2022-06-05 22:24:34 -0300

    Upando projeto final

---
+++ scripttabela.sql
@@ -0,0 +1,35 @@
+create database sneakers;
+
+use sneakers;
+
+create table usuario(
+id INT PRIMARY KEY AUTO_INCREMENT,
+Nome varchar(40),
+DataNasc date,
+email varchar(40),
+senha varchar(40)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+SElect *	from usuario;
+select * from aviso;
+
+SELECT 
+            a.id AS idAviso,
+            a.titulo,
+            a.descricao,
+            a.fk_usuario,
+            u.id AS idUsuario,
+            u.nome,
+            u.email,
+            u.senha
+        FROM aviso a
+            INNER JOIN usuario u
+                ON a.fk_usuario = u.id;
